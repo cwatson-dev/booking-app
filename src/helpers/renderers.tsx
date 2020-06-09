c3ddbf756823e384a4112d2ec1db8f098cddf778
Chris Watson <chriswatson@pm.me>
2020-06-09 11:29:30 +0100

    feat: initial selected doctor screen renderer

--- src/helpers/renderers.tsx
+++ src/helpers/renderers.tsx
@@ -1,6 +1,10 @@
 import React, { Dispatch, SetStateAction } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faMapMarkedAlt, faStar, faUserMd } from '@fortawesome/free-solid-svg-icons';
 
-import { DoctorCard, CardRow } from '../components/cards';
+import { CardRow, DistanceSpan, DoctorCard, RatingSpan, ProfileIconContainer } from '../components/cards';
+import { DoctorProfileContainer } from '../components/structure';
+import { DoctorAbout, DoctorDetail } from '../components/text';
 
 // both renderers could be improved
 // passing `setSelectedDoctor` directly through both funcs to the underlying <DoctorCard> feels wrong
@@ -32,4 +36,34 @@ export const renderDoctorCardsRow = (
     );
   }
   return doctorCardsRows;
+};
+
+// this renderer should be changed to use components with proper spacing css, not `&nbsp;`
+export const renderDoctorDetails = (selectedDoctor: Doctor) => {
+  return (
+    <>
+      <DoctorProfileContainer>
+        <ProfileIconContainer>
+          <FontAwesomeIcon icon={faUserMd} color={'#fff'} size="5x" />
+        </ProfileIconContainer>
+        <DoctorAbout>
+          <DoctorProfileContainer width={'100%'}>
+            <DoctorDetail>
+              Rating:&nbsp;&nbsp;
+              <RatingSpan><FontAwesomeIcon icon={faStar} /> {selectedDoctor.rating}</RatingSpan>
+            </DoctorDetail>
+            <DoctorDetail>
+              Distance:&nbsp;&nbsp;
+              <DistanceSpan><FontAwesomeIcon icon={faMapMarkedAlt} /> {selectedDoctor.distance?.toFixed(2)} km</DistanceSpan>
+            </DoctorDetail>
+          </DoctorProfileContainer>
+          <DoctorDetail width={'80%'}>{selectedDoctor.about}</DoctorDetail>
+        </DoctorAbout>
+      </DoctorProfileContainer>
+      <DoctorDetail>{selectedDoctor.address}</DoctorDetail>
+      <DoctorDetail><a href={`mailto:${selectedDoctor.email}`}>{selectedDoctor.email}</a></DoctorDetail>
+      <DoctorDetail><a href={`tel:${selectedDoctor.phone}`}>{selectedDoctor.phone}</a></DoctorDetail>
+      <DoctorDetail>{selectedDoctor.registered}</DoctorDetail>
+    </>
+  );
 };

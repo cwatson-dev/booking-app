b2defc90dbba4ca46c42bf869f703e46b8a9f7c1
Chris Watson <chriswatson@pm.me>
2020-06-09 10:11:43 +0100

    feat: styled components for structure

---
+++ src/components/structure.ts
@@ -0,0 +1,42 @@
+import styled from 'styled-components';
+
+export const AppContainer = styled.div`
+  background-color: ${(props) => props.theme.background === '#fff' ? '#666' : '#222'};
+  width: 60%;
+  height: 100vh;
+  padding-left: 20%;
+  padding-right: 20%;
+`;
+
+export const ContentContainer = styled.div`
+  background-color: ${(props) => props.theme.background};
+  height: 100vh;
+  flex: 1;
+  box-shadow: ${(props) => props.theme.containerShadow};
+  color: ${(props) => props.theme.text};
+  display: flex;
+  flex-direction: column;
+`;
+
+export const NavContainer = styled.div`
+  flex: 1;
+  display: flex;
+  justify-content: flex-end;
+`;
+
+export const PageContainer = styled.div`
+  flex: 15;
+  display: flex;
+  padding-left: 10%;
+  padding-right: 10%;
+  justify-content: flex-end;
+  flex-direction: column;
+`;
+
+export const HeaderContainer = styled.div`
+  flex: 1;
+  display: flex;
+  flex-direction: row;
+  padding-left: 10%;
+  padding-right: 10%;
+`;

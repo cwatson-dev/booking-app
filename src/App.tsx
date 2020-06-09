d94855db156e22f28d97f92149b2c774ac388679
Chris Watson <chriswatson@pm.me>
2020-06-09 10:18:16 +0100

    feat: header for page, doctors and page state

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,10 @@ import theme from './theme';
 import doctorsData from './assets/doctors.json';
 import { PageButtons, themeSwitchIcon, ThemeSwitcher } from './components/controls';
 import { AppContainer, ContentContainer, HeaderContainer, NavContainer, PageContainer } from './components/structure';
+import { Header, PageDetails, SwitchText } from './components/text';
 const App = () => {
+  const [doctors, setDoctors] = useState<Doctor[]>(doctorsData);
+  const [page, setPage] = useState(0);
   const [themeMode, setThemeMode] = useState<'light' | 'dark'>('light');
   return (
     <div>
@@ -16,6 +19,7 @@ const App = () => {
           <ContentContainer>
             <NavContainer>
               <ThemeSwitcher>
+                <SwitchText>Set theme:&nbsp;&nbsp;</SwitchText>
                 <Switch
                   onChange={(checked) => setThemeMode(checked ? 'light' : 'dark')}
                   checked={themeMode === 'light'}
@@ -27,6 +31,15 @@ const App = () => {
               </ThemeSwitcher>
             </NavContainer>
             <HeaderContainer>
+              <Header>Nearest Doctors</Header>
+              <PageDetails>({page * 9 + 1} to {(page + 1) * 9} of {doctors.length})</PageDetails>
+              <PageButtons
+                setPage={setPage}
+                currentPage={page}
+                maxPage={Math.ceil(doctors.length / 9)}
+                locationEnabled={currentLocationEnabled}
+                setLocationEnabled={setCurrentLocationEnabled}
+              />
             </HeaderContainer>
             <PageContainer>
             </PageContainer>
---
+++ src/components/text.tsx
@@ -0,0 +1,16 @@
+import styled from 'styled-components';
+
+export const SwitchText = styled.p`
+  font-family: 'Roboto Mono';
+`;
+
+export const Header = styled.h2`
+  flex: 1;
+`;
+
+export const PageDetails = styled.p`
+  flex: 1;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;

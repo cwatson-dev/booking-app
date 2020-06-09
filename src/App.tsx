dc078a4dc5a111d2d08f21cf187dd8650dad10b9
Chris Watson <chriswatson@pm.me>
2020-06-09 09:54:50 +0100

    feat: initial themeprovider, thememode state & json data import

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,14 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react';
+import { ThemeProvider } from 'styled-components';
 
+import theme from './theme';
+import doctorsData from './assets/doctors.json';
 const App = () => {
+  const [themeMode, setThemeMode] = useState<'light' | 'dark'>('light');
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <ThemeProvider theme={themeMode === 'light' ? theme.light : theme.dark}>
+      </ThemeProvider>
     </div>
   );
 };
